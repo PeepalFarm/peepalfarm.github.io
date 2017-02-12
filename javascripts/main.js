dea1719d53fb4ebb65884f364f817743bfeef8c2
Robin Singh <locaterobin@gmail.com>
2017-02-13 01:29:01 +0530

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -64,6 +64,7 @@
 			if (!firstload) {
 				FB.XFBML.parse(content_holder);
 			}
+			labnolDOMContentLoaded();
 
 			window.scrollTo(0, 0); 
 			
@@ -174,3 +175,34 @@
 	function getURLParameter(name,searchstring) {
 		return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(searchstring)||[,""])[1].replace(/\+/g, '%20'))||null
 	}
+
+	 /* Light YouTube Embeds by @labnol */
+	    /* Web: http://labnol.org/?p=27941 */
+
+		function labnolDOMContentLoaded() {
+		    var div, n,
+			v = document.getElementsByClassName("youtube-player");
+		    for (n = 0; n < v.length; n++) {
+			div = document.createElement("div");
+			div.setAttribute("data-id", v[n].dataset.id);
+			div.innerHTML = labnolThumb(v[n].dataset.id);
+			div.onclick = labnolIframe;
+			v[n].appendChild(div);
+		    }
+		}
+
+	    function labnolThumb(id) {
+		var thumb = '<img src="https://i.ytimg.com/vi/ID/hqdefault.jpg">',
+		    play = '<div class="play"></div>';
+		return thumb.replace("ID", id) + play;
+	    }
+
+	    function labnolIframe() {
+		var iframe = document.createElement("iframe");
+		var embed = "https://www.youtube.com/embed/ID?autoplay=1";
+		iframe.setAttribute("src", embed.replace("ID", this.dataset.id));
+		iframe.setAttribute("frameborder", "0");
+		iframe.setAttribute("allowfullscreen", "1");
+		this.parentNode.replaceChild(iframe, this);
+	    }
+	     

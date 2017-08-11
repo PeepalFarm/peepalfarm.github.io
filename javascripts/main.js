84bdc1baebb1c7c289078334cce078ab1f24ae23
Robin Singh <locaterobin@gmail.com>
2017-08-11 08:31:00 +0530

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,4 +1,19 @@
 
+
+var marked = require('marked');
+marked.setOptions({
+  renderer: new marked.Renderer(),
+  gfm: true,
+  tables: true,
+  breaks: false,
+  pedantic: false,
+  sanitize: false,
+  smartLists: true,
+  smartypants: false
+});
+
+
+
 	function splitInput(str) {
 		/*
 			* couldn't do YAML file with markdown (content: +++ ##markdown +++) as github seems 
